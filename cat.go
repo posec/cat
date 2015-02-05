a9cca549d0098f065104ab009960a1c784385eaf
David Jones <drj@pobox.com>
2015-02-05 22:10:40 +0000

    Promptly close files

--- cat.go
+++ cat.go
@@ -12,13 +12,15 @@ func main() {
 		io.Copy(os.Stdout, os.Stdin)
 	} else {
 		for _, f := range args {
-			in, err := os.Open(f)
-			if err != nil {
-				fmt.Fprintf(os.Stderr, "%s\n", err)
-				os.Exit(2)
-			}
-			defer in.Close()
-			io.Copy(os.Stdout, in)
+			func() {
+				in, err := os.Open(f)
+				if err != nil {
+					fmt.Fprintf(os.Stderr, "%s\n", err)
+					os.Exit(2)
+				}
+				defer in.Close()
+				io.Copy(os.Stdout, in)
+			}()
 		}
 	}
 }
