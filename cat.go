d5bf74b2be09cbbfc2f8dc430165d0bbc84665d6
David Jones <drj@pobox.com>
2015-02-05 21:50:45 +0000

    Use stdin when there are no arguments

--- cat.go
+++ cat.go
@@ -7,13 +7,18 @@ import (
 )
 
 func main() {
-	for _, f := range os.Args[1:] {
-		in, err := os.Open(f)
-		if err != nil {
-			fmt.Fprintf(os.Stderr, "%s: %s\n", in, err)
-			os.Exit(2)
+	args := os.Args[1:]
+	if len(args) == 0 {
+		io.Copy(os.Stdout, os.Stdin)
+	} else {
+		for _, f := range args {
+			in, err := os.Open(f)
+			if err != nil {
+				fmt.Fprintf(os.Stderr, "%s: %s\n", in, err)
+				os.Exit(2)
+			}
+			defer in.Close()
+			io.Copy(os.Stdout, in)
 		}
-		defer in.Close()
-		io.Copy(os.Stdout, in)
 	}
 }
