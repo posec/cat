857008e995c2dec0f1aae62d7a7b8b34ceec3ea5
David Jones <drj@pobox.com>
2015-02-05 21:43:48 +0000

    Process file arguments

--- cat.go
+++ cat.go
@@ -1,10 +1,19 @@
 package main
 
 import (
-"io"
-"os"
+	"fmt"
+	"io"
+	"os"
 )
 
 func main() {
-    io.Copy(os.Stdin, os.Stdout)
+	for _, f := range os.Args[1:] {
+		in, err := os.Open(f)
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "%s: %s\n", in, err)
+			os.Exit(2)
+		}
+		defer in.Close()
+		io.Copy(os.Stdout, in)
+	}
 }
