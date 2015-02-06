3a7d2fe03547c1be3bc0e874d4ae477ab0d4e721
David Jones <drj@pobox.com>
2015-02-06 19:50:27 +0000

    Avoid two separate io.Copy code paths

--- cat.go
+++ cat.go
@@ -15,33 +15,32 @@ func main() {
 	flag.Parse()
 	args := flag.Args()
 	if len(args) == 0 {
-		io.Copy(os.Stdout, os.Stdin)
-	} else {
-		for _, f := range args {
-			func() {
-				var err error
-				var in *os.File
+		args = []string{"-"}
+	}
+	for _, f := range args {
+		func() {
+			var err error
+			var in *os.File
 
-				if f == "-" {
-					// XCU7: If a file is '-', the cat utility shall read from the standard input at that point in the sequence
-					in = os.Stdin
-				} else {
-					in, err = os.Open(f)
-					if err != nil {
-						fmt.Fprintf(os.Stderr, "%s\n", err)
-						exitStatus = 2
-						return
-					}
-					defer in.Close()
-				}
-				_, err = io.Copy(os.Stdout, in)
+			if f == "-" {
+				// XCU7: If a file is '-', the cat utility shall read from the standard input at that point in the sequence
+				in = os.Stdin
+			} else {
+				in, err = os.Open(f)
 				if err != nil {
 					fmt.Fprintf(os.Stderr, "%s\n", err)
 					exitStatus = 2
 					return
 				}
-			}()
-		}
+				defer in.Close()
+			}
+			_, err = io.Copy(os.Stdout, in)
+			if err != nil {
+				fmt.Fprintf(os.Stderr, "%s\n", err)
+				exitStatus = 2
+				return
+			}
+		}()
 	}
 	os.Exit(exitStatus)
 }
