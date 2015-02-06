fc2f552fca0db0792c35be56484370d8955878b6
David Jones <drj@pobox.com>
2015-02-06 17:18:33 +0000

    Accept - as per POSIX

--- cat.go
+++ cat.go
@@ -17,12 +17,19 @@ func main() {
 	} else {
 		for _, f := range args {
 			func() {
-				in, err := os.Open(f)
-				if err != nil {
-					fmt.Fprintf(os.Stderr, "%s\n", err)
-					os.Exit(2)
+				var in *os.File
+				if f == "-" {
+					// XCU7: If a file is '-', the cat utility shall read from the standard input at that point in the sequence
+					in = os.Stdin
+				} else {
+					var err error
+					in, err = os.Open(f)
+					if err != nil {
+						fmt.Fprintf(os.Stderr, "%s\n", err)
+						os.Exit(2)
+					}
+					defer in.Close()
 				}
-				defer in.Close()
 				io.Copy(os.Stdout, in)
 			}()
 		}
