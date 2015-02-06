c6911ef43360bd664ea3e4241e92722b97cf7c96
David Jones <drj@pobox.com>
2015-02-06 17:23:57 +0000

    As per POSIX, continue processing in face of errors

--- cat.go
+++ cat.go
@@ -10,6 +10,8 @@ import (
 var uIgnored = flag.Bool("u", false, "unbuffered (ignored)")
 
 func main() {
+	var exitStatus int
+
 	flag.Parse()
 	args := flag.Args()
 	if len(args) == 0 {
@@ -17,21 +19,29 @@ func main() {
 	} else {
 		for _, f := range args {
 			func() {
+				var err error
 				var in *os.File
+
 				if f == "-" {
 					// XCU7: If a file is '-', the cat utility shall read from the standard input at that point in the sequence
 					in = os.Stdin
 				} else {
-					var err error
 					in, err = os.Open(f)
 					if err != nil {
 						fmt.Fprintf(os.Stderr, "%s\n", err)
-						os.Exit(2)
+						exitStatus = 2
+						return
 					}
 					defer in.Close()
 				}
-				io.Copy(os.Stdout, in)
+				_, err = io.Copy(os.Stdout, in)
+				if err != nil {
+					fmt.Fprintf(os.Stderr, "%s\n", err)
+					exitStatus = 2
+					return
+				}
 			}()
 		}
 	}
+	os.Exit(exitStatus)
 }
